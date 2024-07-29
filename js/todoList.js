7e512564380c20d6a5d40f86ee84ea8446b0930c
YermIm <iyr7773@naver.com>
2024-07-29 23:28:44 +0900

    feat: 투두 리스트 추가, 삭제 기능

--- js/todoList.js
+++ js/todoList.js
@@ -88,6 +88,7 @@ const nextMonth = () => {
 //   renderCalender();
 // };
 
+//------------------------------------------------------------------------------------------------
 // 투두
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -97,62 +98,104 @@ document.addEventListener("DOMContentLoaded", function () {
   const addTimeInput = document.querySelector(".add-time");
   const addContInput = document.querySelector(".add-cont");
   const delIcon = document.querySelector(".todo-del");
+  const addBtn = document.querySelector(".add-btn");
+  const todoMid = document.querySelector(".todo-mid");
 
-  const addIconSVG1 = `
-		<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M13 0C5.8357 0 0 5.8357 0 13C0 20.1643 5.8357 26 13 26C20.1643 26 26 20.1643 26 13C26 5.8357 20.1643 0 13 0ZM13 2.6C18.7592 2.6 23.4 7.24084 23.4 13C23.4 18.7592 18.7592 23.4 13 23.4C7.24084 23.4 2.6 18.7592 2.6 13C2.6 7.24084 7.24084 2.6 13 2.6ZM11.7 6.5V11.7H6.5V14.3H11.7V19.5H14.3V14.3H19.5V11.7H14.3V6.5H11.7Z" fill="#AEAEAE"/>
-		</svg>
-	`;
+  let addingItem = false;
+  let editingMode = false;
 
-  const addIconSVG2 = `
-		<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-  		<path d="M13 0C5.8201 0 0 5.8201 0 13C0 20.1799 5.8201 26 13 26C20.1799 26 26 20.1799 26 13C26 5.8201 20.1799 0 13 0ZM19.5 14.3H14.3V19.5H11.7V14.3H6.5V11.7H11.7V6.5H14.3V11.7H19.5V14.3Z" fill="#666666"/>
-		</svg>
-	`;
-
-  const editIconSVG1 = `
-		<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M2.84526 23.1507H4.83693L17.1071 10.863L15.1154 8.86849L2.84526 21.1562V23.1507ZM23.1888 8.79726L17.1427 2.8137L19.1343 0.819178C19.6797 0.273059 20.3497 0 21.1445 0C21.9383 0 22.6079 0.273059 23.1533 0.819178L25.1449 2.8137C25.6903 3.35982 25.9748 4.01896 25.9985 4.79112C26.0222 5.56234 25.7614 6.221 25.2161 6.76712L23.1888 8.79726ZM21.126 10.8986L6.04617 26H0V19.9452L15.0799 4.84384L21.126 10.8986ZM16.1113 9.86575L15.1154 8.86849L17.1071 10.863L16.1113 9.86575Z" fill="#AEAEAE"/>
-		</svg>
-	`;
-
-  const editIconSVG2 = `
-		<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M2.84526 23.1507H4.83693L17.1071 10.863L15.1154 8.86849L2.84526 21.1562V23.1507ZM23.1888 8.79726L17.1427 2.8137L19.1343 0.819178C19.6797 0.273059 20.3497 0 21.1445 0C21.9383 0 22.6079 0.273059 23.1533 0.819178L25.1449 2.8137C25.6903 3.35982 25.9748 4.01896 25.9985 4.79112C26.0222 5.56234 25.7614 6.221 25.2161 6.76712L23.1888 8.79726ZM21.126 10.8986L6.04617 26H0V19.9452L15.0799 4.84384L21.126 10.8986ZM16.1113 9.86575L15.1154 8.86849L17.1071 10.863L16.1113 9.86575Z" fill="black"/>
-		</svg>
-	`;
-
-  // 추가 아이콘 클릭했을 때
+  // 플러스아이콘(#todo-add-icon) 클릭했을 때 입력창 토글
   addIcon.addEventListener("click", function () {
-    if (addItem.style.display === "none" || addItem.style.display === "") {
-      addItem.style.display = "flex";
-      addIcon.innerHTML = addIconSVG2;
-      editIcon.innerHTML = editIconSVG1;
-    } else {
-      addItem.style.display = "none";
-      addIcon.innerHTML = addIconSVG1;
+    addingItem = !addingItem;
+    addItem.style.display = addingItem ? "flex" : "none";
+    addIcon.src = addingItem ? "/img/full-plus-btn.png" : "/img/plus-btn.png";
+    addTimeInput.value = "";
+    addContInput.value = "";
 
-      //시간, 내용 초기화
+    // 삭제 버튼 숨기기
+    const delIcon2 = document.querySelectorAll(".todo-del");
+    delIcon2.forEach((icon) => {
+      icon.style.display = "none";
+    });
+  });
+
+  // 추가(.add-btn) 클릭했을 때 목록에 추가
+  // time, content
+  addBtn.addEventListener("click", function () {
+    const time = addTimeInput.value.trim();
+    const content = addContInput.value.trim();
+
+    if (!time || !content) {
+      alert("시간과 내용을 모두 입력해주세요.");
+      return;
+    }
+
+    if (time && content) {
+      const listItemDiv = document.createElement("div");
+      addIcon.src = "/img/plus-btn.png";
+
+      listItemDiv.className = "list-item";
+      listItemDiv.innerHTML = `
+        <img class="todo-check" src="/img/circle.png" />
+        <div class="todo-time">${time}</div>
+        <div class="todo-cont">${content}</div>
+        <img class="todo-del" src="/img/del-icon.png" style="display: none"/>
+      `;
+
+      // 투두리스트 완료 -> 체크버튼 클릭 -> 버튼 변경 & 폰트색상 변경
+      const checkBtn = listItemDiv.querySelector(".todo-check");
+      const todoTimeDiv = listItemDiv.querySelector(".todo-time");
+      const todoContDiv = listItemDiv.querySelector(".todo-cont");
+
+      checkBtn.addEventListener("click", function () {
+        if (checkBtn.src.includes("circle.png")) {
+          checkBtn.src = "/img/full-circle.png";
+          todoTimeDiv.style.color = "#AEAEAE";
+          todoContDiv.style.color = "#AEAEAE";
+        } else {
+          checkBtn.src = "/img/circle.png";
+          todoTimeDiv.style.color = "#ff452b";
+          todoContDiv.style.color = "#000000";
+        }
+      });
+
+      // 목록에 추가 (시간순 정렬)
+      const listItems = document.querySelectorAll(".list-item");
+      let inserted = false;
+      listItems.forEach((item) => {
+        const itemTime = item.querySelector(".todo-time").textContent;
+        if (time < itemTime && !inserted) {
+          item.parentNode.insertBefore(listItemDiv, item);
+          inserted = true;
+        }
+      });
+      if (!inserted) {
+        document.querySelector(".todo-mid").appendChild(listItemDiv);
+      }
+
+      // 시간, 내용 초기화
       addTimeInput.value = "";
       addContInput.value = "";
+      addItem.style.display = "none";
+      addingItem = false;
     }
   });
 
-  //편집 아이콘 클릭했을 때
+  // 편집 아이콘 클릭했을 때
   editIcon.addEventListener("click", function () {
-    if (delIcon.style.display === "none" || delIcon.style.display === "") {
-      delIcon.style.display = "block";
-      editIcon.innerHTML = editIconSVG2;
-    } else {
-      delIcon.style.display = "none";
-      editIcon.innerHTML = editIconSVG1;
+    const delIcons = document.querySelectorAll(".todo-del");
+    delIcons.forEach((icon) => {
+      icon.style.display =
+        icon.style.display === "none" || icon.style.display === "" ? "block" : "none";
+    });
+
+    editingMode = !editingMode;
+    editIcon.src = editingMode ? "/img/full-edit-btn.png" : "/img/big-edit-btn.png";
+  });
+
+  todoMid.addEventListener("click", function (event) {
+    if (event.target.classList.contains("todo-del")) {
+      event.target.closest(".list-item").remove();
     }
-
-    addItem.style.display = "none";
-    addIcon.innerHTML = addIconSVG1;
-
-    //시간, 내용 초기화
-    // addTimeInput.value = "";
-    // addContInput.value = "";
   });
 });
